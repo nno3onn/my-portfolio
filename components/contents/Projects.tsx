1524112f4bde091a4911e07eb11fd1c6362c2435
nno3onn <nno3onn@naver.com>
2023-04-13 17:04:29 +0900

    feat: Projects 프레임 생성

--- components/contents/Projects.tsx
+++ components/contents/Projects.tsx
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import ContentsWrapper from "../common/ContentsWrapper";
+import SectionTitleWrapper from "../common/SectionTitleWrapper";
+import { SectionWrapper } from "../common/SectionWrapper";
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <ProjectsContainer>
+      <SectionWrapper>
+        <SectionTitleWrapper color="white">PROJECTS</SectionTitleWrapper>
+
+        <ContentsContainer>
+          <ContentsWrapper>
+            <>d</>
+          </ContentsWrapper>
+        </ContentsContainer>
+      </SectionWrapper>
+    </ProjectsContainer>
+  );
 };
+
+const ProjectsContainer = styled.div`
+  background-color: ${({ theme }) => theme.backgroundColor.blue};
+`;
+
+const ContentsContainer = styled.div`
+  width: 100%;
+`;
 
 export default Projects;
